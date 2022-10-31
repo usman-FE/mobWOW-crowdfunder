02c892f77d76cd74f52ad2f00218ccbd7feba10a
Muhammad Usman <ch.usman564@gmail.com>
2022-10-31 18:43:54 +0500

    Built Header and working on Hero section

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {},
+  plugins: [],
+};
+
+/*
+
+bodyOverflow: function (modal) {
+  document.querySelector('body').classList.remove('overflow-hidden', 'overflow-visible');
+  document.querySelector('body').classList.add(modal ? 'overflow-hidden' : 'overflow-visible');
+}
+
+*/
+
+
+
